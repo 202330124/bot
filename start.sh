bd3ee9d39e174b6240aa9eb5bcaa6c25f6b729f4
202330124 <202330124@email.daelim.ac.kr>
2026-03-04 22:34:18 +0900

    start.sh 수정

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # 1. lavalink 폴더로 이동하여 실행 (application.yml 인식 목적)
 # 512MB 메모리 제한을 고려하여 자바 메모리 할당량을 256MB로 제한하는 옵션을 추가했습니다.
 cd lavalink
-java -Xmx300M -jar /lavalink/Lavalink.jar &
+java -Xmx300M -jar Lavalink.jar &
 
 # 2. 다시 루트 폴더로 복귀
 cd ..
